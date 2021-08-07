a9230ec58ad8d856feabef056288d426bab5e8b3
Md. Abdullah Ibna Harun <abdullah13426.cse@gmail.com>
2021-08-08 04:36:28 +0600

    Add files via upload

---
+++ mywebapp/src/main/java/com/abdullah/app/App.java
@@ -0,0 +1,11 @@
+package com.abdullah.app;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World..!");
+		
+		List <String> stringList = new ArrayList<String>();
+    }
+}
