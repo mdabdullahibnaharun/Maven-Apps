6fb910042a12be5aba3c733e063793707b1f5a95
Md. Abdullah Ibna Harun <abdullah13426.cse@gmail.com>
2021-08-08 03:19:56 +0600

    Add files via upload

---
+++ MavenTestApp/src/main/java/com/abdullah/app/App.java
@@ -0,0 +1,18 @@
+package com.abdullah.app;
+
+import org.slf4j.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		Logger logger  = LoggerFactory.getLogger(App.class);
+		logger.info("Hello World...");
+		
+    }
+}
